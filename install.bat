7c1f831f2cf7d60464a7c67efbbf6d3058ef6cb5
Felipe Silva <felipe.dacs@gmail.com>
2019-09-11 09:21:31 -0300

    feat: add config especifica gvim

--- install.bat
+++ install.bat
@@ -1,9 +1,11 @@
 DEL %userprofile%\.gitconfig
 DEL %userprofile%\.bash_profile
 DEL %userprofile%\_vimrc
+DEL %userprofile%\_gvimrc
 
 MKLINK /h %userprofile%\.gitconfig %userprofile%\dev\dotfiles\git\.gitconfig
 MKLINK /h %userprofile%\.bash_profile %userprofile%\dev\dotfiles\bash\.bash_profile
 MKLINK /h %userprofile%\_vimrc %userprofile%\dev\dotfiles\vim\vimrc
+MKLINK /h %userprofile%\_gvimrc %userprofile%\dev\dotfiles\vim\gvimrc
 
 PAUSE
