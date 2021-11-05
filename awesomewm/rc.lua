d939d7885adfbdd6fb6b55fe29eb26fa3d96fab9
Felipe Silva <felipe.dacs@gmail.com>
2021-11-05 12:04:38 -0300

    :new:(awesome) diminui comandos disponiveis

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -283,14 +283,14 @@ globalkeys = gears.table.join(
               {description = "focus the previous screen", group = "screen"}),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
-    awful.key({ modkey,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end,
-        {description = "go back", group = "client"}),
+    --awful.key({ modkey,           }, "Tab",
+    --    function ()
+    --        awful.client.focus.history.previous()
+    --        if client.focus then
+    --            client.focus:raise()
+    --        end
+    --    end,
+    --    {description = "go back", group = "client"}),
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
@@ -317,17 +317,17 @@ globalkeys = gears.table.join(
     awful.key({ modkey, i         }, ",", function () awful.layout.inc(-1)                end,
               {description = "select previous", group = "layout"}),
 
-    awful.key({ modkey, "Control" }, "n",
-              function ()
-                  local c = awful.client.restore()
-                  -- Focus restored client
-                  if c then
-                    c:emit_signal(
-                        "request::activate", "key.unminimize", {raise = true}
-                    )
-                  end
-              end,
-              {description = "restore minimized", group = "client"}),
+    --awful.key({ modkey, "Control" }, "n",
+    --          function ()
+    --              local c = awful.client.restore()
+    --              -- Focus restored client
+    --              if c then
+    --                c:emit_signal(
+    --                    "request::activate", "key.unminimize", {raise = true}
+    --                )
+    --              end
+    --          end,
+    --          {description = "restore minimized", group = "client"}),
 
     -- Prompt
     awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
@@ -369,37 +369,37 @@ clientkeys = gears.table.join(
 		awful.client.setmaster(client.focus)
 	end
 	end,  {description = "swap master", group = "client"}),
-    awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
-              {description = "move to screen", group = "client"}),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-              {description = "toggle keep on top", group = "client"}),
-    awful.key({ modkey,           }, "n",
-        function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end ,
-        {description = "minimize", group = "client"}),
-    awful.key({ modkey,           }, "m",
-        function (c)
-            c.maximized = not c.maximized
-            c:raise()
-        end ,
-        {description = "(un)maximize", group = "client"}),
-    awful.key({ modkey, "Control" }, "m",
-        function (c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end ,
-        {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "m",
-        function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end ,
-        {description = "(un)maximize horizontally", group = "client"}),
+    --awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
+    --          {description = "move to screen", group = "client"}),
+    --awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+    --          {description = "toggle keep on top", group = "client"}),
+    --awful.key({ modkey,           }, "n",
+    --    function (c)
+    --        -- The client currently has the input focus, so it cannot be
+    --        -- minimized, since minimized clients can't have the focus.
+    --        c.minimized = true
+    --    end ,
+    --    {description = "minimize", group = "client"}),
+    --awful.key({ modkey,           }, "m",
+    --    function (c)
+    --        c.maximized = not c.maximized
+    --        c:raise()
+    --    end ,
+    --    {description = "(un)maximize", group = "client"}),
+    --awful.key({ modkey, "Control" }, "m",
+    --    function (c)
+    --        c.maximized_vertical = not c.maximized_vertical
+    --        c:raise()
+    --    end ,
+    --    {description = "(un)maximize vertically", group = "client"}),
+    --awful.key({ modkey, "Shift"   }, "m",
+    --    function (c)
+    --        c.maximized_horizontal = not c.maximized_horizontal
+    --        c:raise()
+    --    end ,
+    --    {description = "(un)maximize horizontally", group = "client"}),
 
-    awful.key({ modkey, "Shift"   }, "z", awful.placement.centered,
+    awful.key({ modkey, "Control"   }, "z", awful.placement.centered,
         {description = "center floating window", group = "client"})
 
     --awful.key({ modkey }, "Up", function () awful.util.spawn("xbacklight -dec 15") end,
