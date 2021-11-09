ec7d14689a09758122be7700ee7f44f99696d2a5
Felipe Silva <felipe.dacs@gmail.com>
2021-11-09 17:21:51 -0300

    :bug:(awesome) retorna comando importante pro notion

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -380,12 +380,12 @@ clientkeys = gears.table.join(
     --        c.minimized = true
     --    end ,
     --    {description = "minimize", group = "client"}),
-    --awful.key({ modkey,           }, "m",
-    --    function (c)
-    --        c.maximized = not c.maximized
-    --        c:raise()
-    --    end ,
-    --    {description = "(un)maximize", group = "client"}),
+    awful.key({ modkey,           }, "m",
+        function (c)
+            c.maximized = not c.maximized
+            c:raise()
+        end ,
+        {description = "(un)maximize", group = "client"}),
     --awful.key({ modkey, "Control" }, "m",
     --    function (c)
     --        c.maximized_vertical = not c.maximized_vertical
