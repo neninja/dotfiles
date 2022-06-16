05ecec647fa14d2edfe5f77e328db76223ea5dc6
Felipe Silva <felipe.dacs@gmail.com>
2022-06-16 10:37:18 -0300

    :bug:(nvim) corrige indent do treesitter no php
    - para dart usar :TSEnable indent

--- nvim/lua/pack/treesitter.lua
+++ nvim/lua/pack/treesitter.lua
@@ -8,18 +8,8 @@ require("nvim-treesitter.configs").setup({
     -- additional_vim_regex_highlighting = false,
   },
   indent = {
-    -- enable = false, -- Really breaks stuff if true
-    enable = true
+    enable = false, -- Really breaks stuff if true
   },
-  -- incremental_selection = {
-  -- enable = true,
-  -- keymaps = {
-  -- init_selection = "gnn",
-  -- node_incremental = "grn",
-  -- scope_incremental = "grc",
-  -- node_decremental = "grm",
-  -- },
-  -- },
 })
 
 vim.cmd([[
