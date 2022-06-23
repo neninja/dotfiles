87b4d13370f9711a14769cae0cacdf841fd6b1da
Felipe Silva <felipe.dacs@gmail.com>
2022-06-23 20:30:01 -0300

    :bug:(nvim) corrige deprecated formatting lsp

--- nvim/lua/my/pack/lsp.lua
+++ nvim/lua/my/pack/lsp.lua
@@ -31,7 +31,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>=", "<cmd>lua vim.lsp.buf.format({async=true})<CR>", opts)
 end
 
 -- Remove this in nvim 0.7
