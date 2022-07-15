95ff88ad3b310dd9a31f04702cfb9274092e3c62
Felipe Silva <felipe.dacs@gmail.com>
2022-07-15 00:10:10 -0300

    :bug: corrige problema estranho do instantos

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- THANKS http://lua-users.org/lists/lua-l/2020-01/msg00345.html
 local fullpath = debug.getinfo(1,"S").source:sub(2)
-fullpath = io.popen("realpath '"..fullpath.."'", 'r'):read('a'):gsub('[\n\r]*$','')
+fullpath = io.popen("realpath '"..fullpath.."'", 'r'):read('*all'):gsub('[\n\r]*$','')
 dotfiles_fullpath_nvim, _ = fullpath:match('^(.*/)([^/]-)$')
 vim.cmd('set runtimepath+='..dotfiles_fullpath_nvim)
 
