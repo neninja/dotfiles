4cf7436140df4008009fd06381a59b5ff0dac38d
Felipe Silva <felipe.dacs@gmail.com>
2023-01-30 00:10:19 -0300

    :new:(nvim) copilot em td

--- nvim/lua/my/pack/copilot.lua
+++ nvim/lua/my/pack/copilot.lua
@@ -28,16 +28,7 @@ require('copilot').setup({
     },
   },
   filetypes = {
-    php = true,
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
+    ["."] = true,
   },
   copilot_node_command = 'node', -- Node.js version must be > 16.x
   server_opts_overrides = {},
