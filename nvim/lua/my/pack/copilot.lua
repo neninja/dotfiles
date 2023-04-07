261aab95d4cea3b0acce51e25f67d20d64ad11f6
Felipe Silva <felipe.dacs@gmail.com>
2023-04-07 09:06:30 -0300

    :new:(nvim) remove panel do copilot

--- nvim/lua/my/pack/copilot.lua
+++ nvim/lua/my/pack/copilot.lua
@@ -1,19 +1,5 @@
 require('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
+  panel = { enabled = false },
   suggestion = {
     enabled = true,
     auto_trigger = false,
