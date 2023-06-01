aece7d99aa225617e6368309c4253a5fde9f0a8d
Felipe Silva <felipe.dacs@gmail.com>
2023-06-01 15:37:48 -0300

    :100:(awm) add marker

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -1,3 +1,5 @@
+-- vim: fdm=marker:
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
