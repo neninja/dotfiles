29071292eae044b1db4f9057665d922fb83ee135
Felipe Silva <felipe.dacs@gmail.com>
2023-06-09 21:16:48 -0300

    :100:(nvim) refatora lsp

--- nvim/lua/my/init.lua
+++ nvim/lua/my/init.lua
@@ -123,25 +123,16 @@ nmap('<leader>eq', vim.diagnostic.setloclist, "Show diagnostic on loclist")
 nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
 nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
 nmap('<leader>gs', require('telescope.builtin').lsp_document_symbols, '[G]o to document [S]ymbols')
-nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
 -- See `:help K` for why this keymap
 nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
--- Lesser used LSP functionality
-nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-nmap('<leader>wl', function()
-  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-end, '[W]orkspace [L]ist Folders')
 
 nmap('<leader>=', function()
   if vim.lsp.buf.format then
