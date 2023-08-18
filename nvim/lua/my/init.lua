0121f243ac58aa8befcd1917dea96a41b2d0c4d4
Neni <felipe.dacs@gmail.com>
2023-08-17 22:02:40 -0300

    :new:(nvim) lsp split

--- nvim/lua/my/init.lua
+++ nvim/lua/my/init.lua
@@ -124,6 +124,7 @@ nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
 nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+nmap('gs', ":split | lua vim.lsp.buf.definition()<CR>", '[G]oto Definition [S]plited')
 nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
 nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
