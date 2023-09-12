8b884ca27196144695f1815cc2fa9ff8dd8503e1
Neni <felipe.dacs@gmail.com>
2023-09-12 18:49:02 -0300

    :new:(nvim) aprendi rx_todo do vimwiki hehe

--- nvim/lua/my/pack/vimwiki.lua
+++ nvim/lua/my/pack/vimwiki.lua
@@ -2,6 +2,7 @@ vim.g.vimwiki_list = {
   {
     name = 'my',
     path = '~/vimwiki/my',
+    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|XXX\)\>]],
     auto_generate_links = 1,
     auto_diary_index = 1,
     auto_tags = 1,
@@ -9,6 +10,7 @@ vim.g.vimwiki_list = {
   },
   {
     name = 'bananas 🍌',
+    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|XXX\)\>]],
     path = '~/vimwiki/bananas',
     auto_generate_links = 1,
     auto_diary_index = 1,
@@ -17,6 +19,7 @@ vim.g.vimwiki_list = {
   },
   {
     name = 'idez',
+    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|XXX\)\>]],
     path = '~/vimwiki/idez',
     auto_generate_links = 1,
     auto_diary_index = 1,
@@ -25,6 +28,7 @@ vim.g.vimwiki_list = {
   },
   {
     name = 'winker',
+    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|XXX\)\>]],
     path = '~/vimwiki/winker',
     auto_generate_links = 1,
     auto_diary_index = 1,
