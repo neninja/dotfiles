26efeb5927b1103ab25cdcc474adb522f2c561bd
Neni <felipe.dacs@gmail.com>
2023-09-11 22:19:54 -0300

    :new:(nvim) banana vimwiki

--- nvim/lua/my/pack/vimwiki.lua
+++ nvim/lua/my/pack/vimwiki.lua
@@ -8,6 +8,14 @@ vim.g.vimwiki_list = {
     exclude_files = { 'index.wiki' }
   },
   {
+    name = 'bananas 🍌',
+    path = '~/vimwiki/bananas',
+    auto_generate_links = 1,
+    auto_diary_index = 1,
+    auto_tags = 1,
+    exclude_files = { 'index.wiki' }
+  },
+  {
     name = 'idez',
     path = '~/vimwiki/idez',
     auto_generate_links = 1,
