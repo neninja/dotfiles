6e1f56eac6773190b8dd359ed6e028f0f023141a
Neni <felipe.dacs@gmail.com>
2023-09-13 00:20:02 -0300

    :new:(nvim) experimenta mkdnflow

--- nvim/lua/my/packs.lua
+++ nvim/lua/my/packs.lua
@@ -130,6 +130,119 @@ require('packer').startup(function(use)
     },
     config = function() require('my.pack.telescope') end,
   }
+
+  use {
+    'jakewvincent/mkdnflow.nvim',
+    -- rocks = 'luautf8', -- Ensures optional luautf8 dependency is installed
+    config = function()
+      require('mkdnflow').setup({
+        modules = {
+          bib = true,
+          buffers = true,
+          conceal = true,
+          cursor = true,
+          folds = false,
+          links = true,
+          lists = true,
+          maps = true,
+          paths = true,
+          tables = true,
+          yaml = false
+        },
+        filetypes = {md = true, rmd = true, markdown = true},
+        create_dirs = true,
+        perspective = {
+          priority = 'first',
+          fallback = 'current',
+          root_tell = false,
+          nvim_wd_heel = false,
+          update = false
+        },
+        wrap = false,
+        bib = {
+          default_path = nil,
+          find_in_root = true
+        },
+        silent = false,
+        links = {
+          style = 'markdown',
+          name_is_source = false,
+          conceal = false,
+          context = 0,
+          implicit_extension = nil,
+          transform_implicit = false,
+          transform_explicit = function(text)
+            -- Make lowercase, remove spaces, and reverse the string
+            return string.lower(text:gsub(' ', ''))
+          end
+        },
+        new_file_template = {
+          use_template = false,
+          placeholders = {
+            before = {
+              title = "link_title",
+              date = "os_date"
+            },
+            after = {}
+          },
+          template = "# {{ title }}"
+        },
+        to_do = {
+          symbols = {' ', '-', 'X'},
+          update_parents = true,
+          not_started = ' ',
+          in_progress = '-',
+          complete = 'X'
+        },
+        tables = {
+          trim_whitespace = true,
+          format_on_move = true,
+          auto_extend_rows = false,
+          auto_extend_cols = false
+        },
+        yaml = {
+          bib = { override = false }
+        },
+        mappings = {
+          MkdnEnter = {{'n', 'v'}, '<CR>'},
+          MkdnTab = false,
+          MkdnSTab = false,
+          MkdnNextLink = {'n', '<Tab>'},
+          MkdnPrevLink = {'n', '<S-Tab>'},
+          MkdnNextHeading = {'n', ']]'},
+          MkdnPrevHeading = {'n', '[['},
+          MkdnGoBack = {'n', '<BS>'},
+          MkdnGoForward = {'n', '<Del>'},
+          MkdnCreateLink = false, -- see MkdnEnter
+          MkdnCreateLinkFromClipboard = false, -- see MkdnEnter
+          MkdnFollowLink = false, -- see MkdnEnter
+          MkdnDestroyLink = {'n', '<M-CR>'},
+          MkdnTagSpan = {'v', '<M-CR>'},
+          MkdnMoveSource = {'n', '<F2>'},
+          MkdnYankAnchorLink = {'n', 'yaa'},
+          MkdnYankFileAnchorLink = {'n', 'yfa'},
+          MkdnIncreaseHeading = {'n', '+'},
+          MkdnDecreaseHeading = {'n', '-'},
+          MkdnToggleToDo = {{'n', 'v'}, '<C-Space>'},
+          MkdnNewListItem = false,
+          MkdnNewListItemBelowInsert = {'n', 'o'},
+          MkdnNewListItemAboveInsert = {'n', 'O'},
+          MkdnExtendList = false,
+          MkdnUpdateNumbering =  false,
+          MkdnTableNextCell = {'i', '<Tab>'},
+          MkdnTablePrevCell = {'i', '<S-Tab>'},
+          MkdnTableNextRow = false,
+          MkdnTablePrevRow = {'i', '<M-CR>'},
+          MkdnTableNewRowBelow = false,
+          MkdnTableNewRowAbove = false,
+          MkdnTableNewColAfter = false,
+          MkdnTableNewColBefore = false,
+          MkdnFoldSection = false,
+          MkdnUnfoldSection = false
+        }
+      })
+    end
+  }
 end)
 
 require 'laravim'.setup()
