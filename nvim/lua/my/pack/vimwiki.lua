4741cd1f7d2d4aa25ba3bceb4d641687c7e45f2c
Neni <felipe.dacs@gmail.com>
2023-09-14 01:10:48 -0300

    :bug:(nvim) hi wait qa

--- nvim/lua/my/pack/vimwiki.lua
+++ nvim/lua/my/pack/vimwiki.lua
@@ -2,7 +2,7 @@ vim.g.vimwiki_list = {
   {
     name = 'my',
     path = '~/vimwiki/my',
-    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|WAIT\|XXX\)\>]],
+    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|WAIT\)\>]],
     auto_generate_links = 1,
     auto_diary_index = 1,
     auto_tags = 1,
@@ -10,7 +10,7 @@ vim.g.vimwiki_list = {
   },
   {
     name = 'bananas 🍌',
-    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|WAIT\|XXX\)\>]],
+    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|WAIT\)\>]],
     path = '~/vimwiki/bananas',
     auto_generate_links = 1,
     auto_diary_index = 1,
@@ -19,7 +19,7 @@ vim.g.vimwiki_list = {
   },
   {
     name = 'idez',
-    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|WAIT\|XXX\)\>]],
+    rx_todo = [[\C\<\%(TODO\|DONE\|STARTED\|FIXME\|FIXED\|WAIT REVIEW\|WAIT QA\|WAIT\)\>]],
     path = '~/vimwiki/idez',
     auto_generate_links = 1,
     auto_diary_index = 1,
