706551c3dc0f8dc5e467dd01aab2aa8a76de9bb8
Neni <felipe.dacs@gmail.com>
2023-10-23 02:38:59 -0300

    :new:(nvim) add onsave arquivos go para fmt e imports

--- nvim/lua/my/pack/lsp.lua
+++ nvim/lua/my/pack/lsp.lua
@@ -58,3 +58,27 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+-- ## Aucmd
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go" },
+  callback = function()
+    vim.lsp.buf.format()
+
+    -- organize imports
+    -- THANKS: https://github.com/neovim/nvim-lspconfig/issues/115#issuecomment-1130373799
+    local encoding = vim.lsp.util._get_offset_encoding(vim.api.nvim_get_current_buf())
+    local params = vim.lsp.util.make_range_params(nil, encoding)
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, encoding)
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end,
+})
