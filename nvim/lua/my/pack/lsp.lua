fd043eb140e764901fcd49c47bcd8ba108c44f36
Neni <felipe.dacs@gmail.com>
2023-10-23 02:38:25 -0300

    fix: remove sugestão de snippets de gopls

--- nvim/lua/my/pack/lsp.lua
+++ nvim/lua/my/pack/lsp.lua
@@ -24,6 +24,13 @@ require('neodev').setup()
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities.textDocument = {
+  completion = {
+    completionItem = {
+      snippetSupport = false -- snippets cansados
+    }
+  }
+}
 
 -- Setup mason so it can manage external tooling
 require('mason').setup()
