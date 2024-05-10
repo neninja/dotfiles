6acabeb7e993970c427fd4b31615f44de2eb025c
Neni <felipe.dacs@gmail.com>
2024-05-10 01:28:03 -0300

    :bug:(nvim) fixes setup nvim-tree

--- nvim/lua/my/pack/tree.lua
+++ nvim/lua/my/pack/tree.lua
@@ -15,11 +15,6 @@ require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
     float = {
       enable = false,
     },
